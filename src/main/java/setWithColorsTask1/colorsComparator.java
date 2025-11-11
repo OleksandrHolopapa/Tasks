abe57e66ff5d373eff4df6dce9c1d585803a57e8
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-11-12 01:28:05 +0200

    changed class colorsComparator

--- src/main/java/setWithColorsTask1/colorsComparator.java
+++ src/main/java/setWithColorsTask1/colorsComparator.java
@@ -2,12 +2,10 @@ package setWithColorsTask1;
 
 import java.util.Comparator;
 
-public class colorsComparator implements Comparator {
+public class colorsComparator implements Comparator<Character> {
     private static final String COLORS_ORDER = "ЧОЖЗБСФ";
     @Override
-    public int compare(Object o1, Object o2) {
-        Character color1 = (Character) o1;
-        Character color2 = (Character) o2;
+    public int compare(Character color1, Character color2) {
         int positionOfColor1 = COLORS_ORDER.indexOf(color1);
         int positionOfColor2 = COLORS_ORDER.indexOf(color2);
         return positionOfColor1 - positionOfColor2;
